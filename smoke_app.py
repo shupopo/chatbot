194924799db43457efff713901443756794b7faf
Hiroshima-Shuhei <shuhei.hiroshima@linkx-group.com>
2026-06-08 00:05:10 +0900

    no message

---
+++ smoke_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Startup Check", page_icon="OK")
+
+st.title("Startup Check")
+st.write("Streamlit is running this repository.")
+st.code("smoke_app.py")
